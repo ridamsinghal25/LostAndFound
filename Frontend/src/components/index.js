c2c9e6e92e5ca459df6fc278843e19fde3327c65
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-25 20:58:27 +0530

    add index.js file for component export to codebase

---
+++ Frontend/src/components/index.js
@@ -0,0 +1,7 @@
+import Input from "./Input";
+import Button from "./Button";
+import Loader from "./Loader";
+import Login from "./Login";
+import Signup from "./Signup";
+
+export { Input, Button, Loader, Login, Signup };
