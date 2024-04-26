d0d81919f52704d47cc9403f0abd53da1eb51972
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-26 11:52:13 +0530

    add routes and cookie parser in app file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -14,5 +15,14 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routers/user.route.js";
+import itemRouter from "./routers/item.route.js";
+
+// route declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/items", itemRouter);
 
 export { app };
