0b47b421872839ad6cbc461f82942de89218661d
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-27 17:40:51 +0530

    setup app.jsx file in the codebase in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from "react";
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Header } from "./components";
+import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "./slices/authSlice";
+import { Loader } from "./components/index";
+import authService from "./api/auth";
 
 function App() {
-  return (
+  const [loading, setLoading] = useState(false);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setLoading(true);
+      authService
+        .getCurrentUser()
+        .then((userData) => {
+          if (userData) {
+            const user = userData.data.data;
+            dispatch(login({ user }));
+          } else {
+            dispatch(logout());
+          }
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [isAuthenticated]);
+
+  return !loading ? (
     <>
-      <h1>Lost and found</h1>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
     </>
+  ) : (
+    <Loader />
   );
 }
 
