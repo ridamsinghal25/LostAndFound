5ac2d780430eaf9394e7e00d16e821e85fc84c0d
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-27 21:40:39 +0530

    refactor index.js in frontend

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -3,5 +3,8 @@ import Button from "./Button";
 import Loader from "./Loader";
 import Login from "./Login";
 import Signup from "./Signup";
+import AuthLayout from "./AuthLayout";
+import Header from "./Header";
+import LogoutBtn from "./LogoutBtn";
 
-export { Input, Button, Loader, Login, Signup };
+export { Input, Button, Loader, Login, Signup, AuthLayout, Header, LogoutBtn };
