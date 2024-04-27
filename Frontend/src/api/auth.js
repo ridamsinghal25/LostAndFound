2a37a4bbcd3d75be57db7791d8b2038eee8aa9ef
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-27 21:36:34 +0530

    add api for auth to codebase in frontend

---
+++ Frontend/src/api/auth.js
@@ -0,0 +1,47 @@
+import { apiClient } from "../Interceptors/axios";
+
+class AuthService {
+  createAccount({ fullName, email, password, username, phoneNumber, avatar }) {
+    try {
+      const formData = new FormData();
+      formData.append("fullName", fullName);
+      formData.append("email", email);
+      formData.append("phoneNumber", phoneNumber);
+      formData.append("password", password);
+      formData.append("username", username);
+      formData.append("avatar", avatar[0]);
+
+      return apiClient.post("users/register", formData);
+    } catch (error) {
+      console.log("Error while registering user: ", error);
+    }
+  }
+
+  login({ email, password }) {
+    try {
+      return apiClient.post("users/login", { email, password });
+    } catch (error) {
+      console.log("Error while login user: ", error);
+    }
+  }
+
+  logout() {
+    try {
+      return apiClient.post("users/logout");
+    } catch (error) {
+      console.log("Error while logout usr: ", error);
+    }
+  }
+
+  getCurrentUser() {
+    try {
+      return apiClient.get("users/current-user");
+    } catch (error) {
+      console.log("Error while getting user: ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
