b37858264784c8fa0736962795d902683dce6888
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-28 16:45:25 +0530

    add index.js file for icons export to codebase

---
+++ Frontend/src/assets/index.js
@@ -0,0 +1,17 @@
+import CollectionIcon from "./ItemSVG/CollectionIcon";
+import HomeIcon from "./ItemSVG/HomeIcon";
+import PlusIcon from "./ItemSVG/PlusIcon";
+import SettingIcon from "./ItemSVG/SettingIcon";
+import Item from "./ItemSVG/Item";
+import CancelButtonIcon from "./ItemSVG/CancelButtonIcon";
+import LogoIcon from "./HeaderSVG/LogoIcon";
+
+export {
+  CollectionIcon,
+  HomeIcon,
+  PlusIcon,
+  SettingIcon,
+  Item,
+  CancelButtonIcon,
+  LogoIcon,
+};
