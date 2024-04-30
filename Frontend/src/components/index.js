526ccb3f35c7fac4b439b89388f8ffac3cfeef64
ridamsinghal25 <manusinghal84@gmail.com>
2024-04-30 16:20:19 +0530

    refactor index.js file for components export

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -4,7 +4,22 @@ import Loader from "./Loader";
 import Login from "./Login";
 import Signup from "./Signup";
 import AuthLayout from "./AuthLayout";
-import Header from "./Header";
-import LogoutBtn from "./LogoutBtn";
+import Header from "./Header/Header";
+import AuthButtons from "./Header/AuthButtons";
+import SideBar from "./SideBar";
+import ListItems from "./ListItems";
+import Model from "./Model";
 
-export { Input, Button, Loader, Login, Signup, AuthLayout, Header, LogoutBtn };
+export {
+  Input,
+  Button,
+  Loader,
+  Login,
+  Signup,
+  AuthLayout,
+  Header,
+  AuthButtons,
+  SideBar,
+  ListItems,
+  Model,
+};
