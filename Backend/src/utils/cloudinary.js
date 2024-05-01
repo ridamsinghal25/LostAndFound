4bab9883054ca824de4883d482ce15b5df08d5ef
ridamsinghal25 <manusinghal84@gmail.com>
2024-05-01 16:27:14 +0530

    bugfix in cloudinary file

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -25,12 +25,12 @@ const uploadToCloudinary = async (localFilePath) => {
   }
 };
 
-const deleteFromCloudinary = async (publicId) => {
+const deleteFromCloudinary = async (publicId, resourceType) => {
   try {
     if (!publicId) throw new Error("publicId is required");
 
     const response = await cloudinary.uploader.destroy(publicId, {
-      resource_type: "auto",
+      resource_type: resourceType || "auto",
     });
 
     return response;
