9cef42b65fca5a92874d75f24fcf2b32f6851689
ridamsinghal25 <manusinghal84@gmail.com>
2024-05-08 15:32:29 +0530

    refactor AuthButtons.jsx file

--- Frontend/src/components/Header/AuthButtons.jsx
+++ Frontend/src/components/Header/AuthButtons.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import authService from "../../api/auth.js";
 import { logout } from "../../slices/authSlice.js";
-import { Button } from "../index.js";
+import { Button, Model } from "../index.js";
 import { useNavigate } from "react-router-dom";
 
 function AuthButtons() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [logoutBtn, setLogoutBtn] = useState(false);
   const token = useSelector((state) => state.auth.token);
 
   const authButtons = [
@@ -20,6 +21,14 @@ function AuthButtons() {
       slug: "/signup",
     },
   ];
+
+  function closeModel() {
+    setLogoutBtn(false);
+  }
+
+  function openModel() {
+    setLogoutBtn(true);
+  }
 
   const logoutHandler = () => {
     authService.logout().then(() => {
@@ -39,12 +48,22 @@ function AuthButtons() {
           </Button>
         ))
       ) : (
-        <Button
-          className="mr-1 w-full bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto"
-          onClick={logoutHandler}
-        >
-          Logout
-        </Button>
+        <div className="flex flex-row-reverse gap-4 mb-10px">
+          <Button
+            className="mr-1 w-full bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto"
+            onClick={openModel}
+          >
+            Logout
+          </Button>
+          {logoutBtn && (
+            <Model
+              message={"Do you really want to Logout?"}
+              confirmButton={logoutHandler}
+              closeModel={closeModel}
+              buttonName={"Logout"}
+            />
+          )}
+        </div>
       )}
     </>
   );
